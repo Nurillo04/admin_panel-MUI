f507832c1e2bed1c57907397dfc5341b1a8a6152
Nurillo04 <aminjonovnurillo95@gmail.com>
2024-02-01 11:50:11 +0500

    teachers

--- src/pages/teachers/Teachers.jsx
+++ src/pages/teachers/Teachers.jsx
@@ -33,15 +33,15 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, lastName: "Ahmadaliyev", firstName: "Dilshod", age: 35 },
+  { id: 2, lastName: "Shermatov ", firstName: "Abduhalil", age: 42 },
+  { id: 3, lastName: "Tolibov", firstName: "Ahror", age: 45 },
+  { id: 4, lastName: "Salaydinov", firstName: "Kozim", age: 16 },
+  { id: 5, lastName: "Abdullayev", firstName: "Hasan", age: 23 },
+  { id: 6, lastName: "Turdaliyev", firstName: "Maruf", age: 15 },
+  { id: 7, lastName: "Muhammedov", firstName: "Jahongir", age: 42 },
+  { id: 8, lastName: "Shaydullayev", firstName: "Qobiljon", age: 53 },
+  { id: 9, lastName: "Rasuljonov", firstName: "Sadullo", age: 65 },
 ];
 
 const mappedRows = rows.map((row) => ({ ...row, actions: <Actions /> }));
