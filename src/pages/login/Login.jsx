f11b465b2782ee0296b6e2a7e4f4ec6f771a8d41
Nurillo04 <aminjonovnurillo95@gmail.com>
2024-02-01 13:46:53 +0500

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,53 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./Login.scss";
-const Login = () => {
-  return <div>Login</div>;
+
+const Login = ({ setUser, setName }) => {
+  const { data, setData } = useState;
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    setUser(data);
+    navigate("/profile");
+  };
+  return (
+    <div>
+      <form className="forms">
+        <h1 className="title">Login</h1>
+        <input
+          placeholder="Name..."
+          className="confirm"
+          type="text"
+          name="username"
+          id="username"
+          value={data}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <input
+          placeholder="Username..."
+          className="confirm"
+          type="text"
+          name="username"
+          required
+          id="username"
+          value={data}
+        />
+        <NavLink to="/profile">
+          {/* <button className="btn btns" onClick={handleClick}>
+            Login
+          </button> */}
+          <Button onClick={handleClick} variant="contained">
+            {" "}
+            Login
+          </Button>
+        </NavLink>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,8 +1,27 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Profile.scss";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    navigate("/login");
+  };
+  return (
+    <div className="profil">
+      <h1>Profile Page</h1>
+      <h2>
+        Username : <span>Nurillo</span>
+      </h2>
+      <h2>
+        Password : <span>*********</span>
+      </h2>
+      <Button onClick={handleLogout} variant="contained">
+        LogOut
+      </Button>
+    </div>
+  );
 };
 
 export default Profile;
